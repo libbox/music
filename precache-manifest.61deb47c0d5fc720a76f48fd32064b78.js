facf58dcb8a4a30e42d4d9e1dd025d32875a51f8
kelvin <kelvin@weventure.global>
2020-09-29 15:48:02 +0700

    Updates

---
+++ precache-manifest.61deb47c0d5fc720a76f48fd32064b78.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f997b0892b512f91628242cb2361c0ae",
+    "url": "/music-box/index.html"
+  },
+  {
+    "revision": "3533715bb0d424933f90",
+    "url": "/music-box/static/css/main.193356ff.chunk.css"
+  },
+  {
+    "revision": "664dc4732af0a8ef7de9",
+    "url": "/music-box/static/js/2.b245a9a5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/music-box/static/js/2.b245a9a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3533715bb0d424933f90",
+    "url": "/music-box/static/js/main.246ae904.chunk.js"
+  },
+  {
+    "revision": "30f942bb7783c4b076a0017ec0876fab",
+    "url": "/music-box/static/js/main.246ae904.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "004f0a144b24e8bbeaaa",
+    "url": "/music-box/static/js/runtime-main.03da83d8.js"
+  },
+  {
+    "revision": "dfbf459c7e39ae2e0077b8fa4dc04411",
+    "url": "/music-box/static/media/antoine-barres.dfbf459c.jpg"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/music-box/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/music-box/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "ae11d928aae96245020bb59a1910f758",
+    "url": "/music-box/static/media/clem-onojeghuo-3.ae11d928.jpg"
+  },
+  {
+    "revision": "61c4e0007254eb55e4691291881a4bfb",
+    "url": "/music-box/static/media/clouds.61c4e000.png"
+  },
+  {
+    "revision": "df0cdbfb2c70d9857e3776eb83a5a4eb",
+    "url": "/music-box/static/media/creative-tim-white-slim2.df0cdbfb.png"
+  },
+  {
+    "revision": "facbbdb787d77f975904ba9fa99b15c6",
+    "url": "/music-box/static/media/daniel-olahh.facbbdb7.jpg"
+  },
+  {
+    "revision": "2051e0e847124c536df9c01a3f672032",
+    "url": "/music-box/static/media/erik-lucatero-2.2051e0e8.jpg"
+  },
+  {
+    "revision": "f26c34bb88eb83d5aae99c7dbf3ddd48",
+    "url": "/music-box/static/media/fabio-mangione.f26c34bb.jpg"
+  },
+  {
+    "revision": "7812184c3710d9623c684956fe048ddd",
+    "url": "/music-box/static/media/fog-low.7812184c.png"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/music-box/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "be9fa468a029a195601370492c15519d",
+    "url": "/music-box/static/media/login-image.be9fa468.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/music-box/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/music-box/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/music-box/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/music-box/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
